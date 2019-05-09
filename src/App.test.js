3609a23bf193b4ba54e93f5e4ecd3455369066b5
mani <mani@manis-MBP.hitronhub.home>
2019-05-09 16:57:05 -0700

    remove console logs

--- src/App.test.js
+++ src/App.test.js
@@ -7,16 +7,16 @@ import renderer from 'react-test-renderer';
 test("testing the text shown or not",()=>{
   const tree= renderer.create(<App/>).toJSON();
   const checker= tree.children;
-  console.log( checker);
-    expect(checker[0].type).toEqual('button');
+//  console.log( checker);
+//    expect(checker[0].type).toEqual('button');
 
 
 })
 test("testing button ",()=>{
   const tree= renderer.create(<App/>).toJSON();
   const checker= tree.children;
-  console.log(checker);
-     expect(checker[0].children[1]).toEqual('show');
+//  console.log(checker);
+//     expect(checker[0].children[1]).toEqual('show');
 
 
 })
